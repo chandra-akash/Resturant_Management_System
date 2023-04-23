4cdd655be643ccb6429cb6c647cfb83d380d2819
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2023-04-23 23:32:35 +0530

    dashbord no record found style

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
+import { ImCross } from "react-icons/im";
 
 function Dashboard({ socket }) {
   const [orders, setOrders] = useState([]);
@@ -50,6 +51,32 @@ function Dashboard({ socket }) {
           ))}
         </tbody>
       </Table>
+      {orders.length == 0 ? (
+        <>
+          <div
+            style={{
+              color: "gray",
+              margin: "auto",
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "center",
+              gap: "13px",
+              marginTop: "70px",
+              border: "3px solid gray",
+              width: "fit-content",
+              padding: "20px",
+              borderRadius: "25px",
+              boxShadow:
+                "rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px",
+            }}
+          >
+            <ImCross size={30} />
+            <span style={{ fontSize: "30px", marginTop: "-8px" }}>
+              No Data Found !
+            </span>
+          </div>
+        </>
+      ) : null}
     </>
   );
 }
