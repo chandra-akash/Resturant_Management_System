3abfcac39591bfae5c6558842a2139bf66315f1a
Akash_Chandra <akash@indusnet.co.in>
2023-04-28 10:41:24 +0530

    quantity button added

--- client/src/components/OrderForm.jsx
+++ client/src/components/OrderForm.jsx
@@ -5,11 +5,12 @@ function OrderForm({ socket }) {
   const [tableNumber, setTableNumber] = useState("");
   const [orderBy, setOrderBy] = useState("");
   const [menuItems] = useState([
-    { id: 1, name: "Burger", price: 10 },
-    { id: 2, name: "Pizza", price: 15 },
-    { id: 3, name: "Salad", price: 8 },
+    { id: 1, name: "Burger", price: 10, qty1: 1 },
+    { id: 2, name: "Pizza", price: 15, qty2: 1 },
+    { id: 3, name: "Salad", price: 8, qty3: 1 },
   ]);
   const [selectedOptions, setSelectedOptions] = useState([]);
+  const [ordQty, setOrdQty] = useState(1);
 
   const handleTableNumberChange = (event) => {
     setTableNumber(event.target.value);
@@ -26,6 +27,26 @@ function OrderForm({ socket }) {
     } else {
       setSelectedOptions([...selectedOptions, selectedObject]);
     }
+  };
+
+  const handleIncrement = (e, item) => {
+    console.log("line:33--> ", e, item);
+    setOrdQty(ordQty + 1);
+    item.id == 1
+      ? item.qty1++
+      : item.id == 2
+      ? item.qty2++
+      : item.id == 3
+      ? item.qty3++
+      : null;
+  };
+
+  const handleDecrement = (e, item) => {
+    setOrdQty(ordQty - 1);
+  };
+
+  const handleChange = (event, item) => {
+    setOrdQty(parseInt(event.target.value));
   };
 
   const handleSubmit = (event) => {
@@ -51,16 +72,18 @@ function OrderForm({ socket }) {
     setTableNumber("");
     setOrderBy("");
     setSelectedOptions([]);
+    setOrdQty(1);
   };
 
   return (
     <div className="main">
       <Card className="card">
         <div className="heading">
-          <h2>Welcome to XYZ Resturent</h2>
+          <h2>Welcome to XYZ Restaurant</h2>
         </div>
         <div className="formsection">
           <h1>Place Order</h1>
+
           <form onSubmit={handleSubmit}>
             <Form.Label>Table Number</Form.Label>
             <Form.Control
@@ -71,15 +94,7 @@ function OrderForm({ socket }) {
               value={tableNumber}
               onChange={handleTableNumberChange}
             />
-            {/* <label for="tableNumber">Table Number:</label> <br /> */}
-            {/* <input
-              required
-              type="number"
-              id="tableNumber"
-              value={tableNumber}
-              onChange={handleTableNumberChange}
-            /> */}
-            <Form.Label>Ordered By</Form.Label>
+            <Form.Label className="mt-3">Ordered By</Form.Label>
             <Form.Control
               required
               type="text"
@@ -87,31 +102,11 @@ function OrderForm({ socket }) {
               value={orderBy}
               onChange={handleOrderByChange}
             />
-            {/* <label for="orderby">Ordered By:</label>
-            <br />
-            <input
-              required
-              type="text"
-              id="orderby"
-              value={orderBy}
-              onChange={handleOrderByChange}
-            /> */}
-
             <br />
             <h4>Select Items:</h4>
             <div style={{}}>
               {menuItems.map((item, index) => (
                 <div key={index}>
-                  {/* <input
-                    name={index}
-                    value={JSON.stringify(item)}
-                    type="checkbox"
-                    // onChange={(e) => handleCheck(e, item)}
-                    checked={selectedOptions.some(
-                      (selectedOption) => selectedOption.id === item.id
-                    )}
-                    onChange={() => handleCheckboxChange(item)}
-                  /> */}
                   <div className="itemcheckbox">
                     <Form.Check
                       name={index}
@@ -124,7 +119,31 @@ function OrderForm({ socket }) {
                       onChange={() => handleCheckboxChange(item)}
                     />
                     <span>
-                      {item.name} &#8377;{item.price}/-
+                      {item.name} &#8377;{item.price}/- x
+                      <Form.Group
+                        controlId="countOrderInput"
+                        className="orderQtyCount ms-3 mt-3"
+                      >
+                        <Button
+                          variant="primary"
+                          onClick={(e) => handleIncrement(e, item)}
+                        >
+                          +
+                        </Button>
+                        <Form.Control
+                          type="number"
+                          id={item.id}
+                          value={ordQty}
+                          onChange={(e) => handleChange(e, item)}
+                          style={{ width: "50px" }}
+                        />
+                        <Button
+                          variant="primary"
+                          onClick={(e) => handleDecrement(e, item)}
+                        >
+                          -
+                        </Button>
+                      </Form.Group>
                     </span>
                   </div>
                 </div>
